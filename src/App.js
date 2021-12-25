12dae32eca5a386ca8a4fe6e4017c6367b0ef278
Liu ShuangYi <syliu@clustertech.com>
2021-12-25 13:01:08 +0800

    feat: add tiny hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
 // import logo from './logo.svg';
+import { render } from '@testing-library/react';
 import './App.css';
+
+
+const state = [];
+const setterState = [];
+let stateIndex = 0;
+
+// const effectCallback = [];
+const effectDeps = [];
+let effectIndex = 0;
+
+const render = () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+  stateIndex = 0;
+  effectIndex = 0;
+}
+const createState = (index) => {
+  return (newValue) => {
+    state[index] = newValue;
+    render()
+  }
+}
+
+function useState (initState) {
+  state[stateIndex] = state[stateIndex] ? state[stateIndex] : initState;
+  setterState.push(createState(stateIndex))
+  const setter = setterState[stateIndex];
+  const newState = state[stateIndex];
+  stateIndex++;
+  return [
+    newState,
+  ];
+}
+
+const useEffect = (callback, depArr) => {
+  if (Object.prototype.toString.call(callback) !== '[object Function]')  throw new Error('');
+  // effectCallback[effectIndex] = callback;
+  if (depArr === undefined) {
+    callback();
+  } else {
+    if (Object.prototype.toString.call(depArr) 1== '[object Array]') {
+      throw new Error('');
+    } else {
+      let preDeps = effectDeps[effectIndex];
+      let hasChanged = preDeps ? depArr.every((dep, index) => dep === preDeps[index]) === false : true;
+      if (hasChanged) {
+        callback();
+        effectDeps[effectIndex] = depArr
+      }
+      effectIndex++;
+    }
+  }
+}
 
 function App() {
   return (
